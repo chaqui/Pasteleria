ae7741bf69e7c12b0b1f9a86daa033de3f9d513a
Josue Chaqui <josue.chaqui@gmail.com>
2018-01-19 18:29:01 -0600

    agregando controllers del RecyclerView

---
+++ app/src/main/java/com/ati/queenspasteleria/view/adapter/PictureAdapterCategorias.kt
@@ -0,0 +1,25 @@
+package com.ati.queenspasteleria.view.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.view.View
+/**
+ * Created by josue on 19/01/18.
+ */
+class PictureAdapterCategorias :RecyclerView.Adapter<PictureAdapterCategorias.ViewHolder>(){
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, position: Int): ViewHolder {
+
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
+
+}

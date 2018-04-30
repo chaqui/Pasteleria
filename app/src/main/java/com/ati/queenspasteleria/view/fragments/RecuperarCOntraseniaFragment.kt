325880788852b21bab90e49b70bc77cb9e2eac22
Josue Chaqui <josue.chaqui@gmail.com>
2018-04-30 12:21:10 -0600

    agregando funciones de recuperar contrasenia

---
+++ app/src/main/java/com/ati/queenspasteleria/view/fragments/RecuperarCOntraseniaFragment.kt
@@ -0,0 +1,38 @@
+package com.ati.queenspasteleria.view.fragments
+
+
+import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.ati.queenspasteleria.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RecuperarCOntraseniaFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        var view = inflater!!.inflate(R.layout.fragment_recuperar_contrasenia, container, false)
+        var btRecuperarContasenia = view.findViewById<Button>(R.id.btRecuperarContasenia)
+        var usuarioRecuperar = view.findViewById<TextInputEditText>(R.id.usuarioRecuperar)
+
+        btRecuperarContasenia.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+
+            }
+
+        })
+
+
+        return view
+    }
+
+}// Required empty public constructor

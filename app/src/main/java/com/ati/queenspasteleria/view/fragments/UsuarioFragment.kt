b5a5ee022ac9198a454b1292cda14149cb56649d
Josue Chaqui <josue.chaqui@gmail.com>
2018-05-07 18:33:55 -0600

    iterfaz de usuario terminada

--- app/src/main/java/com/ati/queenspasteleria/view/fragments/UsuarioFragment.kt
+++ app/src/main/java/com/ati/queenspasteleria/view/fragments/UsuarioFragment.kt
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 import com.ati.queenspasteleria.R
+import com.ati.queenspasteleria.Settings.ConfiguracionUsuario
 
 
 /**
@@ -19,7 +22,30 @@ class UsuarioFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_usuario, container, false)
+        var view =  inflater!!.inflate(R.layout.fragment_usuario, container, false)
+        var txWNickName = view.findViewById<TextView>(R.id.txWNickName)
+        var txWNombreApellido = view.findViewById<TextView>(R.id.txWNombreApellido)
+        var txWNit = view.findViewById<TextView>(R.id.txWNit)
+        var txWDireccion = view.findViewById<TextView>(R.id.txWDireccion)
+        var txWTelefono = view.findViewById<TextView>(R.id.txWTelefono)
+        var btCambiarCOntrasenia = view.findViewById<Button>(R.id.btCambiarCOntrasenia)
+
+        var configuracionUsuario = ConfiguracionUsuario()
+
+        val usuario = configuracionUsuario.leerArhivoUsuario(context)
+
+        txWNickName.text = usuario!!.nickname_cli
+        txWNombreApellido.text = usuario!!.nombre_cli+ " "+usuario!!.apellido_cli
+        txWNit.text = usuario!!.nit_cli
+        txWDireccion.text = usuario!!.direccion_cli
+        txWTelefono.text = usuario!!.telefono_cli
+
+        btCambiarCOntrasenia.setOnClickListener {
+
+        }
+
+
+        return view
     }
 
 }// Required empty public constructor
